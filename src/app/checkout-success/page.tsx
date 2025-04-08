396a9dcdd5be7abae3597f91837c6c3b31209def
Monis <monisraza444@gmail.com>
2025-04-09 00:10:36 +0500

    chore: fix checkout-success

--- src/app/checkout-success/page.tsx
+++ src/app/checkout-success/page.tsx
@@ -11,40 +11,41 @@ export const metadata: Metadata = {
     title: "Checkout success"
 }
 
-
 interface PageProps {
-    searchParams: {orderId: string}
+    searchParams: { orderId: string };
 }
 
-export default async function Page({searchParams }: PageProps) {
+export default async function Page({ searchParams }: PageProps) {
+  const { orderId } = searchParams; // No need to await here
 
-  const {orderId} = await searchParams
   const wixClient = await getWixServerClient();
 
   const [order, loggedInMember] = await Promise.all([
     getOrder(wixClient, orderId),
     getLoggedInMember(wixClient),
-  ])
+  ]);
 
-  if(!order) {
+  if (!order) {
     notFound();
   }
 
   const orderCreatedDate = order._createdDate
-  ? new Date(order._createdDate)
-  : null;
-  
-  return (  <main className="mx-auto flex max-w-3xl flex-col items-center space-y-5 px-5 py-10">
-        <h1 className="text-3xl font-bold">We received your order!</h1>
-        <p>A summary of your order was sent to your email address.</p>
-        <h2 className="text-2xl font-bold">Order details</h2>
-        <Order order={order} />
-        {loggedInMember && (
-            <Link href="/profile" className="block text-primary hover:underline">
-                View all your orders
-            </Link>
-        )}
-        {orderCreatedDate && orderCreatedDate.getTime() > Date.now() - 60_000 * 5 && <ClearCart/>}
+    ? new Date(order._createdDate)
+    : null;
+
+  return (
+    <main className="mx-auto flex max-w-3xl flex-col items-center space-y-5 px-5 py-10">
+      <h1 className="text-3xl font-bold">We received your order!</h1>
+      <p>A summary of your order was sent to your email address.</p>
+      <h2 className="text-2xl font-bold">Order details</h2>
+      <Order order={order} />
+      {loggedInMember && (
+        <Link href="/profile" className="block text-primary hover:underline">
+          View all your orders
+        </Link>
+      )}
+      {orderCreatedDate &&
+        orderCreatedDate.getTime() > Date.now() - 60_000 * 5 && <ClearCart />}
     </main>
-    )
+  );
 }
